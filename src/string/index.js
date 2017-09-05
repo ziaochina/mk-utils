221bcec39fcf2d3012dad6b13d9abe68eb2658b1
ziaochina <ziaochina@gmail.com>
2017-09-05 10:55:14 +0800

    zlj

--- src/string/index.js
+++ src/string/index.js
@@ -1,17 +1,22 @@
-function trimLeft(str){
-    return str.replace(/(^\s*)/g,"")
+function trimLeft(str) {
+    return str.replace(/(^\s*)/g, "")
 }
 
-function trimRight(str){
-    return str.replace(/(\s*$)/g,"")
+function trimRight(str) {
+    return str.replace(/(\s*$)/g, "")
 }
 
-function trim(str){
-    return str.replace(/(^\s*)|(\s*$)/g,"")
+function trim(str) {
+    return str.replace(/(^\s*)|(\s*$)/g, "")
+}
+
+function toJson(str) {
+    return (new Function("return " + str))()
 }
 
 export default {
     trimLeft,
     trimRight,
-    trim
+    trim,
+    toJson
 }
