82fc9bd83ac0e4f78407faf5e30d9ab28a0d2987
ziaochina <ziaochina@gmail.com>
2017-09-30 10:34:37 +0800

    zlj

--- src/history/index.js
+++ src/history/index.js
@@ -1,5 +1,90 @@
 import * as history from 'history'
 
+const hashHistory = history.createHashHistory()
+
+const listerners = {}
+
+function listen(selfApp, handler) {
+    if (!listerners[selfApp]) {
+        listerners[selfApp] = []
+    }
+
+    var h = listerners[selfApp].find(o => o.listen == handler)
+    if (!h) {
+        
+        h = handler
+        var unlisten = hashHistory.listen((location, action) => {
+            const childApp = getChildApp(selfApp, location.pathname)
+            handler(childApp, location, action)
+        })
+
+        listerners[selfApp].push({
+            listen: h,
+            unlisten
+        })
+    }
+}
+
+function unlisten(selfApp, handler){
+    if (!listerners[selfApp]) 
+        return
+
+    const index = listerners[selfApp].findIndex(o => o.listen == handler)
+
+    if(index == -1)
+        return 
+
+    listerners[selfApp][index].unlisten()
+    listerners[selfApp].remove(index)
+}
+
+
+function getChildApp(selfApp) {
+    const pathname = hashHistory.location.pathname
+    if (!pathname || pathname == '/' || pathname.indexOf(selfApp) == -1)
+        return 
+
+    const segs = pathname.split('/')
+
+    const selfIndex = segs.findIndex( s => s.indexOf(selfApp) != -1)
+
+    if(segs.length -1 == selfIndex)
+        return
+
+    const ret = segs[selfIndex + 1]
+
+    return ret == '/' ? undefined : ret
+}
+
+function pushChildApp(selfApp, childApp){
+    const pathname = hashHistory.location.pathname
+    if (!pathname || pathname == '/' || pathname.indexOf(selfApp) == -1){
+        hashHistory.push(`/${selfApp}/${childApp}`)
+        return
+    }
+    
+    const segs = pathname.split('/')
+
+    const selfIndex = segs.findIndex( s => s.indexOf(selfApp) != -1)
+
+    if(segs.length -1 == selfIndex){
+        segs.push(childApp)
+    }
+    else{
+        segs[selfIndex + 1] = childApp
+    }
+
+    if(pathname == segs.join('/'))
+        return
+
+    hashHistory.push(segs.join('/'))
+}
+
+
 export default {
-    ...history
+    listen,
+    unlisten,
+    getChildApp,
+    pushChildApp,
+    location: hashHistory.location
 }
