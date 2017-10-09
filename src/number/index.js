ce0d1e37f0f0101cbf880cc4844d9b6789756702
ziaochina <ziaochina@gmail.com>
2017-10-09 13:43:15 +0800

    zlj

--- src/number/index.js
+++ src/number/index.js
@@ -1,4 +1,4 @@
-function getPrecision(value){
+function getPrecision(value) {
     const valueString = value.toString()
     //取e-后字符转换成int,e-10=>10
     if (valueString.indexOf('e-') >= 0) {
@@ -14,6 +14,33 @@ function getPrecision(value){
     return precision
 }
 
+function format(number, decimals,  thousandsSep, decPoint) {
+    number = (number + '').replace(/[^0-9+-Ee.]/g, '')
+
+    var n = !isFinite(+number) ? 0 : +number,
+        prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+        sep = (typeof thousandsSep === 'undefined') ? ',' : thousandsSep,
+        dec = (typeof decPoint === 'undefined') ? '.' : decPoint,  
+        s = '',
+        toFixedFix = function (n, prec) {
+            var k = Math.pow(10, prec)
+            return '' + Math.ceil(n * k) / k
+        }
+
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.')
+    var re = /(-?\d+)(\d{3})/
+    while (re.test(s[0])) {
+        s[0] = s[0].replace(re, "$1" + sep + "$2")
+    }
+
+    if ((s[1] || '').length < prec) {
+        s[1] = s[1] || ''
+        s[1] += new Array(prec - s[1].length + 1).join('0')
+    }
+    return s.join(dec)
+}
+
 export default {
-    getPrecision
+    getPrecision,
+    format
 }
