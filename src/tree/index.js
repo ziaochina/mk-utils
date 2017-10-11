b288f8e5be004586de20cbd4e705192ad96a2706
ziaochina <ziaochina@gmail.com>
2017-10-11 11:23:07 +0800

    zlj

--- src/tree/index.js
+++ src/tree/index.js
@@ -11,6 +11,37 @@ function build(nodes = [], root = { id: 0 }) {
     return root
 }
 
+function find(nodes, childPropName, matchFn){
+    for(let n of nodes){
+        if(matchFn(n) === true){
+            return n
+        }
+
+        if(n[childPropName]){
+            let f = find(n[childPropName], childPropName, matchFn)
+            if(f)
+                return f
+        }
+    }
+    return
+}
+
+function map(nodes, childPropName, newChildPropName, mapFun){
+    var ret = []
+    for(let n of nodes){
+        if(n[childPropName]){
+            n[ newChildPropName || childPropName] = map(n[childPropName], childPropName, newChildPropName, mapFun)
+            if(newChildPropName && newChildPropName != childPropName)
+                delete n[childPropName]
+        }
+        ret.push(mapFun(n))
+    }
+
+    return ret
+}
+
 export default {
-    build
+    build,
+    find,
+    map
 }
