e47ef6192cedf0c6543084f1747c271e0ad71941
ziaochina <ziaochina@gmail.com>
2017-11-14 10:15:08 +0800

    zlj

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import beautify from './beautify'
+import json from './json'
 import fetch from './fetch'
 import number from './number'
 import moment from './moment'
@@ -10,9 +12,12 @@ import string from './string'
 import exception from './exception'
 import tree from './tree'
 import history from './history'
+import less from './less'
 import _ from 'lodash'
 
 export default {
+	beautify,
+	json,
 	fetch,
 	number,
 	string,
@@ -25,5 +30,6 @@ export default {
 	exception,
 	tree,
 	history,
+	less,
 	_
 }
